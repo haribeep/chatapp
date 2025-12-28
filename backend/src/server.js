3d68c0e92b8058c1786d99b50db2230399201790
Hari Murali <harimurali0070@gmail.com>
2025-12-28 22:50:49 +0530

    controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,26 @@ import express from 'express'
 import dotenv from 'dotenv'
 import authrouter from './routes/auth/auth.js'
 import messsageRoute from './routes/message/messge.route.js'
+import path from "path";
 
 dotenv.config()
 
-const app = express()
+const app = express();
+const __dirname = path.resolve(); 
 
 const port = process.env.PORT || 3000
 
 
 app.use( "/api/auth",authrouter)
-app.use( "/api/message",messsageRoute)
+app.use( "/api/message",messsageRoute);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../../frontend/dist")));
+
+    app.get("/.*/", (req,res)=>{
+        res.sendFile(path.join("../../frontend/dist/index.html"))
+    })
+}
 
 app.listen(port, ()=> {
     console.log('server running on port 3000 1233')
